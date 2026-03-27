0cd7ce2b254e9072276a07c06343cf6e13e7c339
Gabe Fontanilla <gfontan1@my.hpu.edu>
2026-03-26 15:26:17 -1000

    fix ngrok stuff

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	server: {
 		host: true,
-		allowedHosts: 'all',
+		allowedHosts: true,
 		proxy: {
 			'/api': {
 				target: 'http://localhost:3001',
